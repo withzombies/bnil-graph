59fb65adf58f38912d3a2d4d366bab3179e76d70
Ryan Stortz <ryan@withzombies.com>
2019-02-06 09:54:55 -0500

    autoformatted with black

--- __init__.py
+++ __init__.py
@@ -26,6 +26,7 @@ show_mapped = False
 # Include SSA in the output
 show_ssa = True
 
+
 def graph_il_insn(g, head, il, label=None):
     # type: (FlowGraph, FlowGraphNode, LowLevelILInstruction, Optional[str]) -> None
 
@@ -33,22 +34,36 @@ def graph_il_insn(g, head, il, label=None):
     tokens = []
 
     if label:
-        tokens.extend([
-            InstructionTextToken(InstructionTextTokenType.KeywordToken, "{}".format(label)),
-            InstructionTextToken(InstructionTextTokenType.OperandSeparatorToken, ": "),
-        ])
+        tokens.extend(
+            [
+                InstructionTextToken(
+                    InstructionTextTokenType.KeywordToken, "{}".format(label)
+                ),
+                InstructionTextToken(
+                    InstructionTextTokenType.OperandSeparatorToken, ": "
+                ),
+            ]
+        )
 
-    if isinstance(il, MediumLevelILInstruction) or isinstance(il, LowLevelILInstruction):
+    if isinstance(il, MediumLevelILInstruction) or isinstance(
+        il, LowLevelILInstruction
+    ):
 
         tokens.append(
-            InstructionTextToken(InstructionTextTokenType.InstructionToken, il.operation.name)
+            InstructionTextToken(
+                InstructionTextTokenType.InstructionToken, il.operation.name
+            )
         )
 
         for i, o in enumerate(il.operands):
             edge_label = il.ILOperations[il.operation][i][0]
             graph_il_insn(g, record, o, edge_label)
     elif isinstance(il, list):
-        tokens.append(InstructionTextToken(InstructionTextTokenType.IntegerToken, "List[{}]".format(len(il))))
+        tokens.append(
+            InstructionTextToken(
+                InstructionTextTokenType.IntegerToken, "List[{}]".format(len(il))
+            )
+        )
 
         for i, item in enumerate(il):
             edge_label = "[{:d}]".format(i)
@@ -56,13 +71,17 @@ def graph_il_insn(g, head, il, label=None):
 
     else:
         if isinstance(il, long):
-            tokens.append(InstructionTextToken(InstructionTextTokenType.IntegerToken, "{:x}".format(il), value=il))
+            tokens.append(
+                InstructionTextToken(
+                    InstructionTextTokenType.IntegerToken, "{:x}".format(il), value=il
+                )
+            )
         else:
-            tokens.append(InstructionTextToken(InstructionTextTokenType.TextToken, str(il)))
+            tokens.append(
+                InstructionTextToken(InstructionTextTokenType.TextToken, str(il))
+            )
 
-
-
-    record.lines = [ DisassemblyTextLine(tokens), ]
+    record.lines = [DisassemblyTextLine(tokens)]
     g.append(record)
     head.add_outgoing_edge(BranchType.UnconditionalBranch, record)
 
@@ -71,18 +90,29 @@ def graph_il(g, head, type, il):
     # type: (FlowGraph, FlowGraphNode, str, LowLevelILInstruction) -> None
 
     il_desc = binaryninja.FlowGraphNode(g)
-    
+
     il_desc.lines = [
         "{}".format(type),
         "",
-        DisassemblyTextLine([
-            InstructionTextToken(InstructionTextTokenType.AddressDisplayToken, "{:#x}".format(il.instr_index), value=il.instr_index),
-            InstructionTextToken(InstructionTextTokenType.OperandSeparatorToken, " @ "),
-            InstructionTextToken(InstructionTextTokenType.AddressDisplayToken, "{:#x}".format(il.address), value=il.address)
-        ]),
-        il.tokens
+        DisassemblyTextLine(
+            [
+                InstructionTextToken(
+                    InstructionTextTokenType.AddressDisplayToken,
+                    "{:#x}".format(il.instr_index),
+                    value=il.instr_index,
+                ),
+                InstructionTextToken(
+                    InstructionTextTokenType.OperandSeparatorToken, " @ "
+                ),
+                InstructionTextToken(
+                    InstructionTextTokenType.AddressDisplayToken,
+                    "{:#x}".format(il.address),
+                    value=il.address,
+                ),
+            ]
+        ),
+        il.tokens,
     ]
-
 
     graph_il_insn(g, il_desc, il)
     g.append(il_desc)
@@ -99,49 +129,49 @@ def graph_ils(bv, g, head, func, addr):
     if show_normal:
         for block in llil:
             for il in block:
-                lookup['LowLevelIL'][il.address].append(il)
+                lookup["LowLevelIL"][il.address].append(il)
 
         for block in mlil:
             for mil in block:
-                lookup['MediumLevelIL'][mil.address].append(mil)
+                lookup["MediumLevelIL"][mil.address].append(mil)
 
     if show_ssa:
         for block in llil.ssa_form:
             for il in block:
-                lookup['LowLevelILSSA'][il.address].append(il)
+                lookup["LowLevelILSSA"][il.address].append(il)
 
         for block in mlil.ssa_form:
             for mil in block:
-                lookup['MediumLevelILSSA'][mil.address].append(mil)
+                lookup["MediumLevelILSSA"][mil.address].append(mil)
 
     if show_mapped:
         mmlil = llil.mapped_medium_level_il
         for block in mmlil:
             for mil in block:
-                lookup['MappedMediumLevelIL'][mil.address].append(mil)
+                lookup["MappedMediumLevelIL"][mil.address].append(mil)
 
         if show_ssa:
             for block in mmlil.ssa_form:
                 for mil in block:
-                    lookup['MappedMediumLevelILSSA'][mil.address].append(mil)
-
+                    lookup["MappedMediumLevelILSSA"][mil.address].append(mil)
 
     for il_type in sorted(lookup):
         ils = lookup[il_type][addr]
         for il in sorted(ils):
             graph_il(g, head, il_type, il)
 
+
 def graph_bnil(bv, addr):
-    blocks = bv.get_basic_blocks_at(addr) # type: List[BasicBlock]
-    function = blocks[0].function # type: Function
+    blocks = bv.get_basic_blocks_at(addr)  # type: List[BasicBlock]
+    function = blocks[0].function  # type: Function
     g = binaryninja.FlowGraph()
 
-    (tokens, ) = [tokens for tokens, insn_addr in function.instructions if insn_addr == addr]
+    (tokens,) = [
+        tokens for tokens, insn_addr in function.instructions if insn_addr == addr
+    ]
 
     head = binaryninja.FlowGraphNode(g)
-    head.lines = [
-        tokens
-    ]
+    head.lines = [tokens]
     g.append(head)
 
     graph_ils(bv, g, head, function, addr)
@@ -149,4 +179,6 @@ def graph_bnil(bv, addr):
     g.show("Instruction Graph ({:#x})".format(addr))
 
 
-PluginCommand.register_for_address("BNIL Instruction Graph", "View BNIL Instruction Information", graph_bnil)
+PluginCommand.register_for_address(
+    "BNIL Instruction Graph", "View BNIL Instruction Information", graph_bnil
+)
